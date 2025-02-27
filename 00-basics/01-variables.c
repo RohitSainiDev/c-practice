2fc1ce1addf13bd1357419da527e1e6cc4247b7e
Rohit Saini <emailrohitsaini@gmail.com>
2025-02-27 10:50:13 +0530

    Add a C program demonstrating variable declaration and naming rules

---
+++ 00-basics/01-variables.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declaring and initialising variables
+    int number = 25;
+    char hash = '#';
+    int age = 25;
+    float g = 9.8;
+
+    // Variable naming rules
+
+    // 1. They are case sensitive
+    int var = 0101;
+    int Var = 0234;
+
+    // 2. First character is alphabet or underscore(_)
+    int _age = 11;
+
+    // 3. No comma or space allowed use underscore to assign meaningful names
+    int final_price = 100;
+
+    // 4. No special character other than underscore allowed. Use meaningful names
+
+    return 0;
+}
