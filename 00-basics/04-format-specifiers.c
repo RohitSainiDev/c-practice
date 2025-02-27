664148678c31c3c20433827becb7a0d098a91ed4
Rohit Saini <emailrohitsaini@gmail.com>
2025-02-27 15:38:04 +0530

    Add a C program demonstrating format specifiers

---
+++ 00-basics/04-format-specifiers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char dollar = '$';
+    printf("Dollar sign is %c \n", dollar); // Format-specifier for character is %c
+
+    int age = 25;
+    printf("Age is %d \n", age); // Format-specifier for integers is %d
+
+    float price = 132.5;
+    printf("Price is %f \n", price); // Format-specifier for floating point numbers is %f
+
+    return 0;
+}
