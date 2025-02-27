e314e2691a9f6a34688efba4a4c464439471849f
Rohit Saini <emailrohitsaini@gmail.com>
2025-02-27 13:32:41 +0530

    Add a C program demonstrating data types and their sizes

---
+++ 00-basics/02-datatypes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    Data Types in C                         Size in bytes
+
+    1. char or signed char                  1
+
+    2. Unsigned char                        1
+
+    3. int or signed int                    2
+
+    4. Unsigned int                         2
+
+    5. Short int or signed short int        2
+
+    6. Unsigned short int                   2
+
+    7. Long int or Signed long int          4
+
+    8. Unsigned long int                    4
+
+    9. float                                4
+
+    10. double                              8
+
+    11. Long double                         10
+    */
+
+    return 0;
+}
