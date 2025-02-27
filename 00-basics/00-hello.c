9a18a3327a56c4bb917904afb3893516bd36d819
Rohit Saini <emailrohitsaini@gmail.com>
2025-02-27 14:14:55 +0530

    Enhance hello.c with comments for clarity and understanding

--- 00-basics/00-hello.c
+++ 00-basics/00-hello.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
+#include <stdio.h> // Preprocessor directive or header file
 
-
-int main()
+int main() // Program execution starts from here
 {
     printf("Hello World in C");
-    return 0;
+    return 0; // There was no error and our program executed successfully
 }
+
+// C is case sensitive. Program execution is from top to down.
