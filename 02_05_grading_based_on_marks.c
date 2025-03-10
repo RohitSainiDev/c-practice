e287e104b89febf547389774b91d177ce826bc43
Rohit Saini <emailrohitsaini@gmail.com>
2025-03-10 17:08:33 +0530

    Add grading program based on marks using if-else ladder and update almanac

---
+++ 02_05_grading_based_on_marks.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks;
+
+    printf("Please enter your marks: \n");
+    scanf("%d", &marks);
+
+    if (marks >= 33 && marks <= 70)
+    {
+        printf("Grade B \n");
+    }
+    else if (marks >= 0 && marks < 33)
+    {
+        printf("Grade C \n");
+    }
+    else if (marks > 70 && marks <= 90)
+    {
+        printf("Grade A \n");
+    }
+    else if (marks > 90 && marks <= 100)
+    {
+        printf("Grade A+ \n");
+    }
+    else
+    {
+        printf("Please enter correct marks(0-100)\n");
+    }
+
+    return 0;
+}
