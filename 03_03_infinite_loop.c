07009e2e7d13a05c283668258eb5587631a4f209
Rohit Saini <emailrohitsaini@gmail.com>
2025-03-11 04:55:23 +0530

    Add infinite loop implementation and correct typo in almanac

---
+++ 03_03_infinite_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // Donot run this code as it might cause your system to behave abnormally
+
+    for (int i = 1;; i++)
+    {
+        print("Infinite loop \n");
+    }
+
+    return 0;
+}
