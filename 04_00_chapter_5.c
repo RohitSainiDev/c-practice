8a46eacaa1b7b2702e1d3a8d9d0d87aa8d5ae9da
Rohit Saini <emailrohitsaini@gmail.com>
2025-03-11 11:33:55 +0530

    Add explanations for functions and recursion in Chapter 5

--- 04_00_chapter_5.c
+++ 04_00_chapter_5.c
@@ -3,5 +3,10 @@
 int main()
 {
     printf("Chapter 5 : Functions & Recursion");
+
+    // Function is a block of code that performs a purticular task. It can be used multiple times and increase code reusability
+
+    // Recursion is a programming technique where a function calls itself to solve a problem by breaking it down into smaller, similar sub-problems.
+
     return 0;
 }
