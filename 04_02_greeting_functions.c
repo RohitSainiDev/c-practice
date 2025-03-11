9b4697956e00e68a2dfc35d796af9041b61030b2
Rohit Saini <emailrohitsaini@gmail.com>
2025-03-11 15:53:01 +0530

    Add greeting functions for Indian and French languages to Chapter 5

---
+++ 04_02_greeting_functions.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void namaste();
+void bonjour();
+
+int main()
+{
+    char ch;
+
+    printf("Enter 'I' for Indian and 'F' for French: \n");
+    scanf("%c", &ch);
+
+    if(ch =='I')
+    {
+        namaste();
+    }
+    else if(ch == 'F')
+    {
+       bonjour();
+    }
+    else
+    {
+        printf("Invalid choice \n");
+    }
+
+    return 0;
+
+}
+
+void namaste()
+{
+    printf("Namaste! \n");
+}
+
+void bonjour()
+{
+    printf("Bonjour! \n");
+}
