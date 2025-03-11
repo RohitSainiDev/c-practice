cbe171921f0c346df4289cb0861697714170790a
Rohit Saini <emailrohitsaini@gmail.com>
2025-03-11 16:13:42 +0530

    Add tax function implementation and update almanac

---
+++ 04_04_tax_function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+float final_price(float value);
+
+
+int main()
+{
+float value =100.0f;
+
+final_price(value);
+printf("The final price is %f\n", final_price(value));
+printf("Value is %f\n", value);
+
+return 0;
+
+}
+
+float final_price(float value)
+{
+     return value+(value*0.18);
+}
